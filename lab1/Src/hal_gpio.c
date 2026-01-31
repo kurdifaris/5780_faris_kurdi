0cf5976298c898b3a2a766835cee82bd6a6919fa
Faris Kurdi <faris.aykurdi@gmail.com>
2026-01-31 00:04:10 -0700

    configure LED pins in My_HAL_GPIO_Init

--- lab1/Src/hal_gpio.c
+++ lab1/Src/hal_gpio.c
@@ -1,11 +1,27 @@
 #include <stdint.h>
 #include <stm32f0xx_hal.h>
 #include <stm32f0xx_hal_gpio.h>
-/*
+
 void My_HAL_GPIO_Init(GPIO_TypeDef  *GPIOx, GPIO_InitTypeDef *GPIO_Init)
 {
+    // Configure LED pins ----
+    // clear bits then set 1 bit for each pin
+    GPIOC->MODER &= ~((3 << 12) | (3 << 14) | (3 << 16) | (3 << 18)); 
+    GPIOC->MODER |=  ((1 << 12) | (1 << 14) | (1 << 16) | (1 << 18));
+
+    // set output type to push-pull
+    GPIOC->OTYPER &= ~((1 << 6) | (1 << 7) | (1 << 8) | (1 << 9));
+
+    // set speed to low
+    GPIOC->OSPEEDR &= ~((3 << 12) | (3 << 14) | (3 << 16) | (3 << 18));
+
+    // set no pull-up, pull-down
+    GPIOC->PUPDR &= ~((3 << 12) | (3 << 14) | (3 << 16) | (3 << 18));
+
+    // Configure USER Button ----
+    
 }
-*/
+
 
 /*
 void My_HAL_GPIO_DeInit(GPIO_TypeDef  *GPIOx, uint32_t GPIO_Pin)
