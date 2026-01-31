c891c2faf24c606ac16744e55e477df8e5a672d1
Faris Kurdi <faris.aykurdi@gmail.com>
2026-01-31 00:11:07 -0700

    Configure User Button

--- lab1/Src/hal_gpio.c
+++ lab1/Src/hal_gpio.c
@@ -6,20 +6,28 @@ void My_HAL_GPIO_Init(GPIO_TypeDef  *GPIOx, GPIO_InitTypeDef *GPIO_Init)
 {
     // Configure LED pins ----
     // clear bits then set 1 bit for each pin
-    GPIOC->MODER &= ~((3 << 12) | (3 << 14) | (3 << 16) | (3 << 18)); 
-    GPIOC->MODER |=  ((1 << 12) | (1 << 14) | (1 << 16) | (1 << 18));
+    GPIOC->MODER &= ~((3 << 12) | (3 << 14) | (3 << 16) | (3 << 18)); // pin 6,7,8,9
+    GPIOC->MODER |=  ((1 << 12) | (1 << 14) | (1 << 16) | (1 << 18)); // set to output 01
 
     // set output type to push-pull
-    GPIOC->OTYPER &= ~((1 << 6) | (1 << 7) | (1 << 8) | (1 << 9));
+    GPIOC->OTYPER &= ~((1 << 6) | (1 << 7) | (1 << 8) | (1 << 9)); // pin 6,7,8,9
 
     // set speed to low
-    GPIOC->OSPEEDR &= ~((3 << 12) | (3 << 14) | (3 << 16) | (3 << 18));
+    GPIOC->OSPEEDR &= ~((3 << 12) | (3 << 14) | (3 << 16) | (3 << 18)); // pin 6,7,8,9
 
     // set no pull-up, pull-down
-    GPIOC->PUPDR &= ~((3 << 12) | (3 << 14) | (3 << 16) | (3 << 18));
+    GPIOC->PUPDR &= ~((3 << 12) | (3 << 14) | (3 << 16) | (3 << 18)); // pin 6,7,8,9
 
     // Configure USER Button ----
-    
+    // set mode to input 00
+    GPIOA->MODER &= ~(3 << 0); // pin 0
+
+    // set speed to low
+    GPIOA->OSPEEDR &= ~(3 << 0); // pin 0
+
+    // set no pull-up, pull-down (clear and then set to 10)
+    GPIOA->PUPDR &= ~(3 << 0);
+    GPIOA->PUPDR |=  (2 << 0); 
 }
 
 
