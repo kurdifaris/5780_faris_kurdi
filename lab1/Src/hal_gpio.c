a56142e707fcb2d738d061b99dff58101f984d14
Faris Kurdi <faris.aykurdi@gmail.com>
2026-02-02 18:44:47 -0700

    implemented togglepin and readpin

--- lab1/Src/hal_gpio.c
+++ lab1/Src/hal_gpio.c
@@ -37,12 +37,15 @@ void My_HAL_GPIO_DeInit(GPIO_TypeDef  *GPIOx, uint32_t GPIO_Pin)
 }
 */
 
-/*
+
 GPIO_PinState My_HAL_GPIO_ReadPin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
 {
-    return -1;
+    if (GPIOx->IDR & GPIO_Pin) {
+        return GPIO_PIN_SET;
+    }
+    return GPIO_PIN_RESET;
 }
-*/
+
 
 
 void My_HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState)
@@ -58,6 +61,7 @@ void My_HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState 
 
 void My_HAL_GPIO_TogglePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
 {
-    
+    // toggle by XORing the output data register
+    GPIOx->ODR ^= GPIO_Pin;
 }
 
