19f522f7152ad97706113c749f16e4cec7efa831
Faris Kurdi <faris.aykurdi@gmail.com>
2026-02-02 19:17:03 -0700

    implemented debouncer logic and updated main.c and system_setup.c

--- lab1/Src/lab1.c
+++ lab1/Src/lab1.c
@@ -17,18 +17,31 @@ int main(void)
 
   HAL_RCC_GPIOC_CLK_Enable(); // enable GPIOC clock
   assert(RCC->AHBENR & RCC_AHBENR_GPIOCEN);
+  HAL_RCC_GPIOA_CLK_Enable(); // enable GPIOA clock
+  assert(RCC->AHBENR & RCC_AHBENR_GPIOAEN);
 
   GPIO_InitTypeDef initSt = {GPIO_PIN_8 | GPIO_PIN_9, GPIO_MODE_OUTPUT_PP, GPIO_SPEED_FREQ_LOW, GPIO_NOPULL};
   HAL_GPIO_Init(GPIOC, &initSt); // initialize pc8 and pc9
   assert((GPIOC->MODER & (0xF << 16)) == (0x5 << 16));
 
   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET); // start pc8 high
-  assert(GPIOC->ODR == 0x00000100);
+  // assert(GPIOC->ODR == 0x00000100);
+
+  uint32_t debouncer = 0;
 
   while (1)
   {
-    HAL_Delay(200); // delay 200ms
-    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8 | GPIO_PIN_9); // toggle pc8 and pc9
+    debouncer = (debouncer << 1);
+
+    if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) == GPIO_PIN_SET) {
+      debouncer |= 0x01;
+    }
+
+    if (debouncer == 0x7FFFFFFF) {
+      HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8 | GPIO_PIN_9); // toggle pc8 and pc9
+    }
+
+    HAL_Delay(10);
   }
 }
 
--- lab1/Src/system_setup.c
+++ lab1/Src/system_setup.c
@@ -5,3 +5,8 @@ void HAL_RCC_GPIOC_CLK_Enable(void) {
     // enable GPIOC clock
     RCC->AHBENR |= RCC_AHBENR_GPIOCEN; 
 }
+
+void HAL_RCC_GPIOA_CLK_Enable(void) {
+    // enable GPIOA clock
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN; 
+}
