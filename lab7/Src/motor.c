6e3e45ef1099325dd49bee72a6e8428d2c822e7b
Faris Kurdi <faris.aykurdi@gmail.com>
2026-03-30 16:18:41 -0600

    finish PI_update implementation, add Segger files

--- lab7/Src/motor.c
+++ lab7/Src/motor.c
@@ -209,9 +209,11 @@ void PI_update(void) {
     error_integral = error_integral + (Ki * error);
     /// TODO: Clamp the value of the integral to a limited positive range
     if (error_integral < 0) {
-        error_integral = 0;}
+        error_integral = 0;
+    }
     if (error_integral > 3200) {
-        error_integral = 3200;}
+        error_integral = 3200;
+    }
     /* Hint: The value clamp is needed to prevent excessive "windup" in the integral.
      *       You'll read more about this for the post-lab. The exact value is arbitrary
      *       but affects the PI tuning.
@@ -237,9 +239,16 @@ void PI_update(void) {
      * required for tuning.
      */
 
-     /// TODO: Divide the output into the proper range for output adjustment
+    /// TODO: Divide the output into the proper range for output adjustment
+    output = output / 32;
 
-     /// TODO: Clamp the output value between 0 and 100
+    /// TODO: Clamp the output value between 0 and 100
+    if (output < 0) {
+        output = 0;
+    }
+    if (output > 100) {
+        output = 100;
+    }
 
     pwm_setDutyCycle(output);
     duty_cycle = output;            // For debug viewing
